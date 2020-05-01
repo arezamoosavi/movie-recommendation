01330362184c322da2d1cd9c0ed4df00bc49181a
alireza <sdamoosavi@gmail.com>
2020-05-01 18:18:55 +0430

    flask_admin works!

--- src/application.py
+++ src/application.py
@@ -1,34 +1,38 @@
 from flask_mongoengine import MongoEngine, MongoEngineSessionInterface
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
+from flask_admin import Admin 
+from flask_admin.contrib.mongoengine import ModelView
 from resources import Recommend
-from models import db
+from models import db, SearchMovie
 import config
 
 app = Flask(__name__)
 api = Api(app)
 
-app.config['SECRET_KEY'] = config.SECRET_KEY
+app.config['SECRET_KEY'] = 'seeecccreet'
 app.config['UPLOAD_FOLDER'] = config.UPLOAD_FOLDER
 
 
+#db
 app.config['MONGODB_HOST'] = 'mongodbhost'
 app.config['MONGODB_PORT'] = 27017
 app.config['MONGODB_DB'] = 'admin'
 app.config['MONGODB_USERNAME'] = 'root'
 app.config['MONGODB_PASSWORD'] = 'rootpassword'
 
-# app.config['MONGO_HOST'] = 'db'
-# app.config['MONGO_PORT'] = '27017'
-# app.config['MONGO_DBNAME'] = 'admin'
-# app.config['MONGO_USERNAME'] = 'root'
-# app.config['MONGO_PASSWORD'] = 'rootpassword'
-# app.config['MONGO_AUTH_SOURCE'] = 'admin'
-
 db.init_app(app)
 app.session_interface = MongoEngineSessionInterface(db)
 
+
+# admin
+admin = Admin(app)
+admin.add_view(ModelView(SearchMovie))
+
+
+#endpoints
 api.add_resource(Recommend, '/recommend')
+
 if __name__=="__main__":
     app.run(host=config.HOST,
             port=config.HOST,
